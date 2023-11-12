9b1e65154ab9be14f17a524c3d28d480f4e7be8a
vo_o <vo_o@etna-alternance.net>
2023-11-12 15:17:59 +0100

    [ADD] => Ajout d'une fonction qui crée les tâches avec le formulaire

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,16 @@ function showForm(element) {
     /**Fonction qui affiche le forumaire si il y a un clique sur le bouton */
     const form = document.querySelector("form");
     const section = document.querySelector("section.new-todo");
-    //   alert(element.innerHTML);
-    //   form.style.visibility = "visible";
-    //   section.style.visibility = "visible";
-    //   element.innerHTML = "Cancel";
-    //   element.addEventListener("click", function () {
-    //     alert(element.innerHTML);
-    //     if (element.innerHTML == "Cancel") {
-    //       form.style.visibility = "hidden";
-    //       section.style.visibility = "hidden";
-    //       element.innerHTML = "Create task";
-    //     }
-    //   });
     if (element.innerHTML === "Create task") {
         form.style.visibility = "visible";
         section.style.visibility = "visible";
+        section.style.display = "initial";
         element.innerHTML = "Cancel";
     }
     else {
         form.style.visibility = "hidden";
         section.style.visibility = "hidden";
+        section.style.display = "none";
         element.innerHTML = "Create task";
     }
 }
@@ -38,6 +28,43 @@ function getTask() {
     for (const data of datas) {
         res.push(data.value);
     }
-    alert(res);
+    alert(res[0]);
+    createTask(res);
     return res;
+}
+function saveDatas() {
+    sessionStorage.setItem("tasks", toDoList.innerHTML);
+}
+function createTask(datas) {
+    /**Fonction qui crée des bloc de tâches à réaliser */
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    const divInfos = document.createElement("div");
+    const span0 = document.createElement("span");
+    const span1 = document.createElement("span");
+    span0.innerHTML = "\u00d7";
+    span1.innerHTML = "stylou";
+    div.className = "tasks";
+    divInfos.className = "infos";
+    divInfos.appendChild(span0);
+    divInfos.appendChild(span1);
+    const title = document.createElement("h3");
+    title.innerHTML = `${datas[0]}`;
+    const date = document.createElement("p");
+    date.innerHTML = `${datas[1]}`;
+    div.appendChild(divInfos);
+    div.appendChild(title);
+    div.appendChild(date);
+    li.appendChild(div);
+    toDoList.appendChild(li);
+    saveDatas();
+}
+function deleteTask(element) {
+    var _a;
+    (_a = element.parentElement) === null || _a === void 0 ? void 0 : _a.remove();
+    saveDatas();
+}
+const toDoList = document.getElementById("list");
+if (toDoList != undefined) {
+    toDoList.innerHTML = sessionStorage.getItem("tasks");
 }
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ function showForm(element: HTMLButtonElement) {
   if (element.innerHTML === "Create task") {
     form.style.visibility = "visible";
     section.style.visibility = "visible";
+    section.style.display = "initial";
     element.innerHTML = "Cancel";
   } else {
     form.style.visibility = "hidden";
     section.style.visibility = "hidden";
+    section.style.display = "none";
     element.innerHTML = "Create task";
   }
 }
@@ -26,6 +28,45 @@ function getTask() {
   for (const data of datas) {
     res.push(data.value);
   }
-  alert(res);
+  alert(res[0]);
+  createTask(res);
   return res;
+}
+
+function saveDatas() {
+  sessionStorage.setItem("tasks", toDoList.innerHTML);
+}
+function createTask(datas: any) {
+  /**Fonction qui crée des bloc de tâches à réaliser */
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  const divInfos = document.createElement("div");
+  const span0 = document.createElement("span");
+  const span1 = document.createElement("span");
+  span0.innerHTML = "\u00d7";
+  span1.innerHTML = "stylou";
+  div.className = "tasks";
+  divInfos.className = "infos";
+  divInfos.appendChild(span0);
+  divInfos.appendChild(span1);
+
+  const title = document.createElement("h3");
+  title.innerHTML = `${datas[0]}`;
+  const date = document.createElement("p");
+  date.innerHTML = `${datas[1]}`;
+  div.appendChild(divInfos);
+  div.appendChild(title);
+  div.appendChild(date);
+  li.appendChild(div);
+  toDoList.appendChild(li);
+  saveDatas();
+}
+
+function deleteTask(element: HTMLSpanElement) {
+  element.parentElement?.remove();
+  saveDatas();
+}
+const toDoList = document.getElementById("list") as HTMLUListElement;
+if (toDoList != undefined) {
+  toDoList.innerHTML = sessionStorage.getItem("tasks")!;
 }
