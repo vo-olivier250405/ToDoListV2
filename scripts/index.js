6e38d3ce539525d23d6cd3ec542366595e6c8d10
vo_o <vo_o@etna-alternance.net>
2023-11-12 12:29:38 +0100

    [ADD] => index.ts =>  Affichage du formulaire avec le bouton

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,43 @@
 "use strict";
-function showForm() {
+function showForm(element) {
+    /**Fonction qui affiche le forumaire si il y a un clique sur le bouton */
     const form = document.querySelector("form");
     const section = document.querySelector("section.new-todo");
-    form.style.visibility = "visible";
-    section.style.visibility = "visible";
+    //   alert(element.innerHTML);
+    //   form.style.visibility = "visible";
+    //   section.style.visibility = "visible";
+    //   element.innerHTML = "Cancel";
+    //   element.addEventListener("click", function () {
+    //     alert(element.innerHTML);
+    //     if (element.innerHTML == "Cancel") {
+    //       form.style.visibility = "hidden";
+    //       section.style.visibility = "hidden";
+    //       element.innerHTML = "Create task";
+    //     }
+    //   });
+    if (element.innerHTML === "Create task") {
+        form.style.visibility = "visible";
+        section.style.visibility = "visible";
+        element.innerHTML = "Cancel";
+    }
+    else {
+        form.style.visibility = "hidden";
+        section.style.visibility = "hidden";
+        element.innerHTML = "Create task";
+    }
 }
-function getDatas() {
+function getTask() {
+    /**Fonction qui renvoie les données du formulaire */
     const datas = [
         document.getElementById("name"),
         document.getElementById("date"),
         document.getElementById("description"),
+        document.getElementById("priority"),
     ];
     const res = Array();
     for (const data of datas) {
         res.push(data.value);
     }
     alert(res);
+    return res;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
-function showForm() {
+function showForm(element: HTMLButtonElement) {
+  /**Fonction qui affiche le forumaire si il y a un clique sur le bouton */
   const form: HTMLFormElement = document.querySelector("form")!;
   const section: any = document.querySelector("section.new-todo")!;
-  form.style.visibility = "visible";
-  section.style.visibility = "visible";
+
+  if (element.innerHTML === "Create task") {
+    form.style.visibility = "visible";
+    section.style.visibility = "visible";
+    element.innerHTML = "Cancel";
+  } else {
+    form.style.visibility = "hidden";
+    section.style.visibility = "hidden";
+    element.innerHTML = "Create task";
+  }
 }
 
-function getDatas() {
+function getTask() {
+  /**Fonction qui renvoie les données du formulaire */
   const datas: HTMLInputElement[] = [
     document.getElementById("name") as HTMLInputElement,
     document.getElementById("date") as HTMLInputElement,
     document.getElementById("description") as HTMLInputElement,
+    document.getElementById("priority") as HTMLInputElement,
   ];
   const res = Array();
   for (const data of datas) {
     res.push(data.value);
   }
+  alert(res);
   return res;
 }
