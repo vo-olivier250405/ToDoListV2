15e9fad62d8d076cd0ad1ff85825ec063deb7916
vo_o <vo_o@etna-alternance.net>
2023-11-12 11:05:03 +0100

    [ADD] => index.ts => Récupération des données du formulaire

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("hello world");
+function showForm() {
+    const form = document.querySelector("form");
+    const section = document.querySelector("section.new-todo");
+    form.style.visibility = "visible";
+    section.style.visibility = "visible";
+}
+function getDatas() {
+    const datas = [
+        document.getElementById("name"),
+        document.getElementById("date"),
+        document.getElementById("description"),
+    ];
+    const res = Array();
+    for (const data of datas) {
+        res.push(data.value);
+    }
+    alert(res);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello world");
+function showForm() {
+  const form: HTMLFormElement = document.querySelector("form")!;
+  const section: any = document.querySelector("section.new-todo")!;
+  form.style.visibility = "visible";
+  section.style.visibility = "visible";
+}
+
+function getDatas() {
+  const datas: HTMLInputElement[] = [
+    document.getElementById("name") as HTMLInputElement,
+    document.getElementById("date") as HTMLInputElement,
+    document.getElementById("description") as HTMLInputElement,
+  ];
+  const res = Array();
+  for (const data of datas) {
+    res.push(data.value);
+  }
+  return res;
+}
